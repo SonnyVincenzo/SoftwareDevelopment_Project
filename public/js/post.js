895de15ee2512971e43c7bbb838b513ca6cfaa46
Jennifer Taylor <jeataylor.2003@gmail.com>
2026-04-08 14:58:32 +0200

    updated ids

--- public/js/post.js
+++ public/js/post.js
@@ -21,3 +21,21 @@ shareButton.addEventListener("click", async () => {
         pageRouter.get('/home', handleHome);
     }
 });
+
+shareButtonFunction() {
+    const req = {
+        body: {
+            postHeader: postTitle.value,
+            postBody: postBody.value
+        }
+    }
+    // wait for the handle to handle the post request
+    await handlePostPost(req, res);
+    if (res.value === "500 Internal Server Error") {
+        alert('500 Internal Server Error');
+    } 
+    else {
+        pageRouter.get('/home', handleHome);
+    }
+
+}
