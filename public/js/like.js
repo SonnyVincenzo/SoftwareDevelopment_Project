7e7d710fe33d87b1ff0debad5779e9b5a21075b9
Jennifer Taylor <jeataylor.2003@gmail.com>
2026-04-14 19:35:21 +0200

    functioning dislike and counter

--- public/js/like.js
+++ public/js/like.js
@@ -4,10 +4,22 @@ for (let i = 0; i < button.length; i++) {
   let currButton = button[i];
   let currLikes = likes[i];
   currButton.addEventListener("click", () => {
-    console.log(button);
     currButton.style.color = "rgba(1, 199, 249, 1)";
     value = parseInt(currLikes.getAttribute('value')) + 1;
     currLikes.setAttribute('value', value)
     currLikes.innerHTML = value;
+  });
+}
+
+const dislikeButton = document.getElementsByClassName("dislike");
+const dislikes = document.getElementsByClassName("dislikes");
+for (let i = 0; i < button.length; i++) {
+  let currDislikeButton = dislikeButton[i];
+  let currDislikes = dislikes[i];
+  currDislikeButton.addEventListener("click", () => {
+    currDislikeButton.style.color = "rgba(1, 199, 249, 1)";
+    value = parseInt(currDislikes.getAttribute('value')) + 1;
+    currDislikes.setAttribute('value', value)
+    currDislikes.innerHTML = value;
   });
 }
