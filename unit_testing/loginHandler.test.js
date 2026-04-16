d14867b8bd8f21b4b6e4965327e6f19d76780898
LJkiller <OfficialLJkiller@gmail.com>
2026-04-16 18:32:54 +0200

    Unit testing for loginHandler In addition slight changes to enable unit testing.
    - connection.js: added "/promise" for async & await functionality.
    - loginHandler.js:
        - logic for finding user and redirection;
        - HTTP status code 400 and 401 for missing form data or incorrect data.
    - loginHandler.test.js: Unit testing for:
        - Wrong username input;
        - Wrong password input;
        - Successfull login & redirection check.
    
    logicHandler.js' login logic needs to be further developed as it only manages to find the user, no actual login.

--- unit_testing/loginHandler.test.js
+++ unit_testing/loginHandler.test.js
@@ -1,1 +1,63 @@
-console.log('Test');
+import { describe, it, after } from 'node:test';
+import assert from 'node:assert';
+import { createLoginPostHandler } from '../private/routeHandlers/auth/loginHandler.js';
+import db from '../private/db/connection.js';
+
+describe('Login POST handler.', () => {
+    after(async () => {
+        await db.end();
+    });
+
+    const createReq = (username, password) => ({
+        body: { username, password }
+    });
+    const createRes = (expectation) => ({
+        writeHead: (statusCode, headers) => {
+            assert.strictEqual(statusCode, expectation.statusCode);
+            assert.deepStrictEqual(headers, { 'Content-Type': expectation.contentType || 'text/plain' });
+        },
+        write: (message) => {
+            assert.strictEqual(message, expectation.message);
+        },
+        end: () => {
+        }
+    });
+
+    it('Wrong username, return 401.', async () => {
+        const req = createReq('124', '123');
+        const res = createRes({ statusCode: 401, message: 'Invalid username.' });
+
+        const handler = createLoginPostHandler(db);
+        await handler(req, res);
+    });
+
+    it('Wrong password, return 401.', async () => {
+        const req = createReq('123', '124');
+        const res = createRes({ statusCode: 401, message: 'Incorrect password.' });
+
+        const handler = createLoginPostHandler(db);
+        await handler(req, res);
+    });
+
+    it("URL redirect to user's endpoint, login successful.", async () => {
+        const req = createReq('123', '123');
+        let redirectedTo = null;
+        const res = {
+            redirect: (url) => {
+                redirectedTo = url;
+            }
+        };
+
+        db.execute = (query, params) => {
+            assert.strictEqual(query, 'SELECT username, password FROM User WHERE username = ?');
+            assert.deepStrictEqual(params, ['123']);
+            return Promise.resolve([[{ username: '123', password: '123' }]]);
+        };
+
+        const handler = createLoginPostHandler(db);
+        await handler(req, res);
+
+        assert.strictEqual(redirectedTo, '/user/123');
+    });
+
+});
